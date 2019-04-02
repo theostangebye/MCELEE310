65aead8ba02b554718c5f870956927ffe92f7b29
Theo@Plato <theostangebye@gmail.com>
2019-04-02 15:55:31 -0400

    gen files

---
+++ lab8v2.X/mcc_generated_files/custom_type_defs.h
@@ -0,0 +1,22 @@
+/* 
+ * File:   custom_type_defs.h
+ * Author: theostangebye
+ *
+ * Created on April 2, 2019, 12:21 AM
+ */
+
+#ifndef CUSTOM_TYPE_DEFS_H
+#define	CUSTOM_TYPE_DEFS_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+typedef unsigned char uint8_t;
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* CUSTOM_TYPE_DEFS_H */
+
