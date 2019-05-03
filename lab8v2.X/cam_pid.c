40a23304c651b32fd8af350679a96a60840bd6e4
Theo@Plato <theostangebye@gmail.com>
2019-05-03 13:42:11 -0400

    Untested PID

---
+++ lab8v2.X/cam_pid.c
@@ -0,0 +1,51 @@
+
+uint8_t sp;
+uint8_t last_error;
+float pg;
+float dg;
+
+pid_init(uint8_t setpoint, float pgain, float dgain) {
+    sp = setpoint;
+    pg = pgain;
+    dg = dgain;
+    last_error = 0;
+}
+
+/**
+ * Generates a PID response based on finding the transition point from 
+ * white to dark.  (inside border of outside lane marking).  
+ * We compute the derivative of the pixel array and find the 
+ * minimum derivative value.
+ * The location of the minimum derivative value is the process value.
+ * The error is setpoint - process value.
+ * @param pixels
+ * @return 
+ */
+int_8t pid_getResponse(uint8_t* pixels) {
+    
+    /**
+     * Search for boarder in cam array. 
+     */
+    int16_t der_min = 0;
+    uint_8t der_min_loc = 0;
+    int16_t der = 0;
+    for (int i = 0; i < 126; i++) {
+        der = pixels[i+1] - pixels[i]; // compute derivative of pixel value
+        if (der < der_min) {
+            // we have a new minimum derivative value 
+            // the greatest minimum derivative should be where the track goes 
+            // from white to black.
+            der_min = der;
+            der_min_loc = i;
+        }
+        
+        
+        // PID Calculations
+        uint8_t error = sp - der_min_loc;       // Compute error (setpoint - pv)
+        int16_t der_error = error - last_error; // compute derivative of error
+        
+        last_error = error;
+        
+        return (int8_t) pg*error + dg * der_error; // compute pid response
+    }
+}
--- lab8v2.X/cam_pid.h
+++ lab8v2.X/cam_pid.h
@@ -12,8 +12,18 @@
 extern "C" {
 #endif
 
-    pid_init();
+    pid_init(uint8_t setpoint);
     
+    /**
+     * Generates a PID response based on finding the transition point from 
+     * white to dark.  (inside border of outside lane marking).  
+     * We compute the derivative of the pixel array and find the 
+     * minimum derivative value.
+     * The location of the minimum derivative value is the process value.
+     * The error is setpoint - process value.
+     * @param pixels
+     * @return 
+     */
     int_8t pid_getResponse(uint8_t* pixels);
 
 
